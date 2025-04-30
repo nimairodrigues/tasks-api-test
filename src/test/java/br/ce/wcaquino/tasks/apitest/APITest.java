e6b855ad5d13ea5de01de631075e9a9501ae2c00
Nimai Rodrigues <nimaizao@gmail.com>
2025-04-30 17:41:47 -0300

    teste de remoção

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -52,6 +52,31 @@ public class APITest {
 				.body("message", CoreMatchers.is("Due date must not be in past"))
 		;
 	}
+	
+	@Test
+	public void deveRemoverTarefaComSucesso() {
+		//inserir
+		Integer id = RestAssured
+			.given()
+				.body("{\"task\": \"TarefaTeste\", \"dueDate\": \"2030-10-10\"}")
+				.contentType(ContentType.JSON)
+			.when()
+				.post("/todo")
+			.then()
+				.log().all()
+				.statusCode(201)
+				.extract().path("id")
+		;
+		System.out.println(id);
+		//remover
+		RestAssured
+			.given()
+			.when()
+				.delete("/todo/"+id)
+			.then()
+				.statusCode(204)
+		;
+	}
 }
 
 
